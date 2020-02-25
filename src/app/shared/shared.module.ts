64230f8ce4904411bba2aabb341d1f7c39a7ffa0
balcomiv <35431931+balcomiv@users.noreply.github.com>
2020-02-25 06:12:34 -0500

    Prettier cfg and header/footer start

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [],
+})
+export class SharedModule { }
